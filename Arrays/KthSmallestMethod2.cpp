767c9839f281e2c7f69d0e6787cdd87f146e68c4
shashidhar-gr <shashidhargr26@gmail.com>
2019-01-18 20:18:23 +0530

    Added Kth smallest element second method.

--- Arrays/KthSmallestMethod2.cpp
+++ Arrays/KthSmallestMethod2.cpp
@@ -10,10 +10,10 @@ Write your code in this editor and press "Run" button to compile and execute it.
 using namespace std;
 void insertElement( int );
 int heap[50] = {};
-int arr[6] = {6, 7, 12, 10, 15, 17};
+int arr[4] = {6, 7, 12, 10};
 int arraySize = sizeof(arr)/sizeof(*arr);
 int heapSize = sizeof(heap)/sizeof(*heap);
-int pos = 0, first = 0, last = 0;
+int pos = 1, first = 1, last = 1;
 
 int main() {
     cout<<"Array size: "<<arraySize<<"\n";
@@ -25,31 +25,35 @@ int main() {
 }
 
 void insertElement(int num) {
+    cout<<"Inserting "<<num<<"\n";
     pos = last;
     heap[pos] = num;
     last++;
     int comp = 0, temp = 0;
-    
+    cout<<"last: "<<last<<", pos: "<<pos<<"\n";
     while(pos > first && pos != 0) {
-        
+        //cout<<"In loop.....";
         comp = pos/2;
+        //cout<<"comp: "<<comp<<"\n";
+        //cout<<"comparing "<<heap[comp]<<" and "<<heap[pos]<<"\n";
         if(heap[comp] < heap[pos]) {
+            cout<<"swapping "<<heap[comp]<<" and "<<heap[pos]<<"\n";
             temp = heap[comp];
             heap[comp] = heap[pos];
             heap[pos] = temp;
+            
+            cout<<"position "<<comp<<" has "<<heap[comp]<<" and position "<<pos<< " has "<<heap[pos]<<"\n";
+        
             pos = comp;
+            
+            cout<<"new pos is "<<pos<<"\n";
+            
         }
-        pos = last;
+        //pos = last;
     }
-    
+    //cout<<"Last before printing array: "<<last<<"\n";
     for(int i = 0; i < last; i++) {
         cout<<heap[i]<<" ";
     }
     cout<<"\n";
 }
-
-
-
-
-
-
