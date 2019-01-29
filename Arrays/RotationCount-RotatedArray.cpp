9edd8fff2a5807d2c9f8af347b482d0b07f0c1ee
shashidhar-gr <shashidhargr26@gmail.com>
2019-01-29 21:44:47 +0530

    Still minor bugs are there, will fix this soon!

--- Arrays/RotationCount-RotatedArray.cpp
+++ Arrays/RotationCount-RotatedArray.cpp
@@ -20,7 +20,32 @@ Output: 0
 
 #include <iostream>
 using namespace std;
+int findPivotPosition(int[], int, int, int);
 
 int main() {
+	
+	int a[6] = {15, 18, 2, 3, 6, 12};
+	int n = sizeof(a)/sizeof(*a);
+	
+	int pivotPosition = findPivotPosition(a, n, 0, n-1);
+	cout<<"Pivot position: "<<pivotPosition<<"\n";
+	cout<<"Number of rotations: "<<pivotPosition;
 	return 0;
+}
+
+int findPivotPosition(int a[], int n, int low, int high) {
+ 
+	int mid = 0;
+	if( low <= high ) {
+		mid = (low + high)/2;
+		
+		if(a[mid] > a[mid + 1])
+			return (mid + 1)%n;
+		
+		if(a[low] > a[mid])
+			return findPivotPosition(a, n, low, mid-1);
+		else
+			return findPivotPosition(a, n, mid + 1, high);
+	}
+	return -1;
 }
