357e379b65d84d0b325f03100d4402098ac0bd54
shashidhar-gr <shashidhargr26@gmail.com>
2019-01-29 20:45:48 +0530

    added problem description

--- Arrays/KthSmallestMethod2.cpp
+++ Arrays/KthSmallestMethod2.cpp
@@ -1,10 +1,16 @@
-/******************************************************************************
+/*
+K’th Smallest/Largest Element in Unsorted Array | Set 1.
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+Given an array and a number k where k is smaller than size of array, we need to find the k’th smallest element in the given array. It is given that ll array elements are distinct.
 
-*******************************************************************************/
+Input: arr[] = {7, 10, 4, 3, 20, 15}
+       k = 3
+Output: 7
+
+Input: arr[] = {7, 10, 4, 3, 20, 15}
+       k = 4
+Output: 10
+*/
 
 #include <iostream>
 using namespace std;
