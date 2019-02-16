39e1740b1353b3a3bc31a1c32ff11bfc48fd8e49
shashidhar-gr <shashidhargr26@gmail.com>
2019-02-16 09:51:10 +0530

    Minimum heap problem, kth smallest problem

--- Arrays/Order Statistics/KthSmallest-MimimumHeap.cpp
+++ Arrays/Order Statistics/KthSmallest-MimimumHeap.cpp
@@ -14,11 +14,97 @@ Output: 10
 
 */
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 void printArray(int[], int);
+void insertElement(int, int);
+void removeElement();
+int a[6] = {7, 10, 4, 3, 20, 15};
+int b[6] = {};
+int heapSize = sizeof(b)/sizeof(*b);
 
 int main() {
+	int pos = 0;
+	int aSize = sizeof(a)/sizeof(*a);
+	
+	for(int i = 0; i < aSize; i++) {
+		insertElement(i, a[i]);
+	}
+	
+	int k = 3;
+	
+	for(int i = 0; i < k -1; i++) {
+		removeElement();
+	}
+	cout<<"\n"<<k<<" smallest element is "<<b[0]<<"\n";
+	return 0;
+}
 
+
+/*
+Function to insert new element to heap and also heapify.
+*/
+void insertElement(int pos, int element) {
+
+	b[pos] = element;
+	int temp = 0, parent = (pos-1)/2;
+	
+	while(b[pos] < b[parent] && pos >= 0) {
+		temp = b[pos];
+		b[pos] = b[parent];
+		b[parent] = temp;
+		parent = (parent-1)/2;
+		pos = (pos-1)/2;
+	}
+	
+	return;
+}
+
+/*
+Function to delete root element and also heapify.
+*/
+void removeElement() {
+	int temp = 0, k = 0, leftNode = 0, rightNode = 0;
+	
+	if(heapSize <= 0)
+		return;
+	
+	//Swapping last element with root.
+	temp = b[k];
+	b[k] = b[heapSize-1];
+	b[heapSize-1] = temp;
+	
+	//Decreasing heap size by one.
+	heapSize = heapSize - 1;
+	
+	//Heapifying after removing root element.
+	while( (k*2)+1 < heapSize && (k*2)+2 < heapSize ) {
+		leftNode = (k*2)+1;
+		rightNode = (k*2)+2;
+		if( b[k] > b[leftNode] && b[leftNode] < b[rightNode] )  {
+			temp = b[k];
+			b[k] = b[leftNode];
+			b[leftNode] = temp;
+			k = leftNode;
+		}
+		else {
+			temp = b[k];
+			b[k] = b[rightNode];
+			b[rightNode] = temp;
+			k = rightNode;
+		}		
+	}
+	
+	leftNode = (k*2)+1;
+	if(leftNode < heapSize && b[k] > b[leftNode] ) {
+		temp = b[k];
+		b[k] = b[leftNode];
+		b[leftNode] = temp;
+	}
+	
+	printArray(b, heapSize);
+
+	return;
 }
 
 //Generic method to print array elements.
@@ -29,3 +115,4 @@ void printArray( int a[], int n ) {
 	cout<<"\n";
 	return;
 }
+
--- Arrays/Order Statistics/MiminumHeap.cpp
+++ Arrays/Order Statistics/MiminumHeap.cpp
@@ -1,6 +1,5 @@
 /*
-
-
+Minimum heap problem.
 */
 
 #include <iostream>
@@ -8,35 +7,31 @@
 using namespace std;
 void printArray(int[], int);
 void insertElement(int, int);
+void removeElement();
 int a[12] = {3, 1, 0, 5, 6, 4, 7, 8, 2, -2, -1, -3};
 int b[12] = {};
+int heapSize = sizeof(b)/sizeof(*b);
 
 int main() {
 	int pos = 0;
-	//int a[5] = {1, 3, 6, 2, 0};
 	int aSize = sizeof(a)/sizeof(*a);
 	
 	for(int i = 0; i < aSize; i++) {
 		insertElement(i, a[i]);
 	}
 	
-	int bSize = sizeof(b)/sizeof(*b);
-	printArray(b, bSize);
+	int k = 2;
 	
+	for(int i = 0; i < k -1; i++) {
+		removeElement();
+	}
+	cout<<"\n"<<k<<" smallest element is "<<b[0]<<"\n";
 	return 0;
 }
 
-//Generic method to print array elements.
-void printArray( int a[], int n ) {
-	for( int i = 0; i < n; i++ ) {
-		cout<<a[i]<<"\t";
-	}
-	cout<<"\n";
-	return;
-}
 
 /*
-
+Function to insert new element to heap and also heapify.
 */
 void insertElement(int pos, int element) {
 
@@ -53,3 +48,60 @@ void insertElement(int pos, int element) {
 	
 	return;
 }
+
+/*
+Function to delete root element and also heapify.
+*/
+void removeElement() {
+	int temp = 0, k = 0, leftNode = 0, rightNode = 0;
+	
+	if(heapSize <= 0)
+		return;
+	
+	//Swapping last element with root.
+	temp = b[k];
+	b[k] = b[heapSize-1];
+	b[heapSize-1] = temp;
+	
+	//Decreasing heap size by one.
+	heapSize = heapSize - 1;
+	
+	//Heapifying after removing root element.
+	while( (k*2)+1 < heapSize && (k*2)+2 < heapSize ) {
+		leftNode = (k*2)+1;
+		rightNode = (k*2)+2;
+		if( b[k] > b[leftNode] && b[leftNode] < b[rightNode] )  {
+			temp = b[k];
+			b[k] = b[leftNode];
+			b[leftNode] = temp;
+			k = leftNode;
+		}
+		else {
+			temp = b[k];
+			b[k] = b[rightNode];
+			b[rightNode] = temp;
+			k = rightNode;
+		}		
+	}
+	
+	leftNode = (k*2)+1;
+	if(leftNode < heapSize && b[k] > b[leftNode] ) {
+		temp = b[k];
+		b[k] = b[leftNode];
+		b[leftNode] = temp;
+	}
+	
+	printArray(b, heapSize);
+
+	return;
+}
+
+//Generic method to print array elements.
+void printArray( int a[], int n ) {
+	for( int i = 0; i < n; i++ ) {
+		cout<<a[i]<<"\t";
+	}
+	cout<<"\n";
+	return;
+}
+
