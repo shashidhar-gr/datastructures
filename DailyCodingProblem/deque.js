ed4a7c086c03b33d5dfc10885f6e192ebc5e0daf
shashidhar-gr <shashidhargr26@gmail.com>
2019-09-05 13:42:28 +0530

    Implementing deque

---
+++ DailyCodingProblem/deque.js
@@ -0,0 +1,47 @@
+/**
+ * The deque abstract data type is defined by the following structure and operations. 
+ * A deque is structured, as described below, as an ordered collection of items where items are added and removed from either end, either front or rear
+ * Reference: https://runestone.academy/runestone/books/published/pythonds/BasicDS/TheDequeAbstractDataType.html
+ */
+
+class Deque {
+    
+    constructor() {
+        this.data = [];
+    }
+    
+    //Adds a new item to the front of the deque. It needs the item and returns nothing.
+    addFront(item) {
+        this.data.push(item)
+    }
+
+    //Adds a new item to the rear of the deque. It needs the item and returns nothing.
+    addRear(item) {
+        this.data.unshift(item)
+    }
+
+    //Removes the front item from the deque. It needs no parameters and returns the item. The deque is modified.
+    removeFront() {
+        return this.data.pop()
+    }
+
+    //Removes the rear item from the deque. It needs no parameters and returns the item. The deque is modified.
+    removeRear() {
+        return this.data.shift()
+    }
+
+    //Tests to see whether the deque is empty. It needs no parameters and returns a boolean value.
+    isEmpty() {
+        if(this.data.length === 0)
+            return true;
+        else 
+            return false;
+    }
+
+    //Returns the number of items in the deque. It needs no parameters and returns an integer.
+    size() {
+        return this.data.length;
+    }
+}
+
+exports.Deque = Deque;
---
+++ DailyCodingProblem/test/deque.spec.js
@@ -0,0 +1,68 @@
+var Deque = require('../deque').Deque;
+var assert = require('assert');
+
+describe('Deque', function() {
+    it('should return proper object.', function() {
+        let deque = new Deque();   
+        assert.equal(deque instanceof Object, true)
+    })
+    it('should return object of type Deque.', function() {
+        let deque = new Deque();   
+        assert.equal(deque instanceof Deque, true)
+    })
+    it('should return True since deque is empty.', function() {
+        let deque = new Deque();   
+        assert.equal(deque.isEmpty(), true)
+    })
+    it('should return 0 since deque is empty.', function() {
+        let deque = new Deque();   
+        assert.equal(deque.size(), 0)
+    })
+    it('should add element to front of the deque.', function() {
+        let deque = new Deque();
+        deque.addFront(1)
+        deque.addFront(2)
+        assert.equal(deque.isEmpty(), false)   
+        assert.equal(deque.size(), 2)
+    })
+    it('should remove element from front of the deque.', function() {
+        let deque = new Deque();
+        deque.addFront(10)
+        deque.addFront("Shashi")
+        assert.equal(deque.size(), 2)
+        assert.equal(deque.removeFront(), "Shashi")   
+        assert.equal(deque.size(), 1)
+    })
+    it('should add element to rear of the deque.', function() {
+        let deque = new Deque();
+        deque.addRear(1)
+        deque.addRear(2)
+        assert.equal(deque.isEmpty(), false)   
+        assert.equal(deque.size(), 2)
+    })
+    it('should remove element from rear of the deque.', function() {
+        let deque = new Deque();
+        deque.addRear(10)
+        deque.addRear("Shashi")
+        assert.equal(deque.size(), 2)
+        assert.equal(deque.removeRear(), "Shashi")   
+        assert.equal(deque.size(), 1)
+    })
+    it('should remove element from rear of the deque.', function() {
+        let deque = new Deque();
+        deque.addRear(10)
+        deque.addRear("Shashidhar")
+        deque.addFront(20)
+        deque.addFront("GR")
+        assert.equal(deque.size(), 4)
+        assert.equal(deque.removeRear(), "Shashidhar")   
+        assert.equal(deque.size(), 3)
+        assert.equal(deque.removeFront(), "GR")
+        assert.equal(deque.size(), 2)
+        assert.equal(deque.removeRear(), 10)
+        assert.equal(deque.size(), 1)
+        assert.equal(deque.removeRear(), 20)
+        assert.equal(deque.size(), 0)
+        assert.equal(deque.isEmpty(), true)
+    })
+})
